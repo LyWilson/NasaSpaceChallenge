6da7b43d47928aea9200d1eb4f34c7f60682a589
dvp536 <missdegat@hotmail.com>
2024-10-05 12:32:42 -0400

    Add a background image and have a better style on the page overall

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,20 @@
+import "./App.css";
 function App() {
-    return <h1>REACT APP</h1>
+    return <div>
+        <img src={"./pictures/SpaceBackground.jpg"}></img>
+        <div className="titleContainer">
+            <label>Seismic Detection Across the Solar System</label>
+        </div>
+        <div className="container">
+            <div className="tabs">
+                <input type="radio" id="radioLunar" name="tabs" checked />
+                <label className="tab" htmlFor="radioLunar">Lunar</label>
+                <input type="radio" id="radioMars" name="tabs" />
+                <label className="tab" htmlFor="radioMars">Mars</label>
+                <span className="glider"/>
+            </div>
+        </div>
+    </div>
 }
 
 export default App
