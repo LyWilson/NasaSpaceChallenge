92118a475283106656f316f1800661275ee9cb96
Wilson Ly <e2254323@cmaisonneuve.qc.ca>
2024-10-06 19:34:17 -0400

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
     return (
         <div>
-            <Router>
+            <Router basename="/NasaSpaceChallenge">
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/mars" element={<MarsPage />} />
